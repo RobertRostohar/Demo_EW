9006ebe9ab1bdaf045046ed9f7d93854845f9ac1
Robert Rostohar <robert.rostohar@arm.com>
2022-05-19 13:55:01 +0200

    Separate peripheral memmap and irq per board

---
+++ EdgeImpulse/bsp/AVH_Corstone-300_U55/peripheral_memmap.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2021 Arm Limited. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// Auto-generated file
+// ** DO NOT EDIT **
+
+#ifndef PERIPHERAL_MEMMAP_H
+#define PERIPHERAL_MEMMAP_H
+
+/******************************************************************************/
+/*                         Peripheral memory map                              */
+/******************************************************************************/
+
+#define ETHOS_U_NPU_BASE           (0x48102000)    /* Ethos-U NPU base address*/
+/* #undef ETHOS_U_NPU_TA0_BASE */
+/* #undef ETHOS_U_NPU_TA1_BASE */
+
+/******************************************************************************/
+/*                      Secure Peripheral memory map                          */
+/******************************************************************************/
+
+/* #undef SEC_ETHOS_U_NPU_BASE */
+/* #undef SEC_ETHOS_U_NPU_TA0_BASE */
+/* #undef SEC_ETHOS_U_NPU_TA1_BASE */
+
+#endif /* PERIPHERAL_MEMMAP_H */
