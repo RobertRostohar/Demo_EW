95b893521ac5301e0af26db16122b1e15a8bac9f
Robert Rostohar <robert.rostohar@arm.com>
2022-05-25 10:28:50 +0200

    Add EdgeImpulse run scripts for AVH target

---
+++ EdgeImpulse/avh_run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+VHT_Corstone_SSE-300_Ethos-U55 -C ethosu.num_macs=256 -C mps3_board.visualisation.disable-visualisation=1 -C mps3_board.telnetterminal0.start_telnet=0 -C mps3_board.uart0.out_file=- -C mps3_board.uart0.shutdown_on_eot=1 -a Objects/example.axf $*
