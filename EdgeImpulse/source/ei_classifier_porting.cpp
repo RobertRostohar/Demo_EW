633efdbf12c1b0b5e9b0fb94e809e300b983b615
Robert Rostohar <robert.rostohar@arm.com>
2022-06-03 08:46:38 +0200

    Update EdgeImpulse model

--- EdgeImpulse/source/ei_classifier_porting.cpp
+++ EdgeImpulse/source/ei_classifier_porting.cpp
@@ -46,6 +46,11 @@ __attribute__((weak)) EI_IMPULSE_ERROR ei_run_impulse_check_canceled()
  */
 __attribute__((weak)) EI_IMPULSE_ERROR ei_sleep(int32_t time_ms)
 {
+    auto start = ei_read_timer_ms();
+    if(time_ms<0) { 
+        return EI_IMPULSE_OK; 
+    }
+    while(ei_read_timer_ms() - start < static_cast<uint32_t>(time_ms));
     return EI_IMPULSE_OK;
 }
 
