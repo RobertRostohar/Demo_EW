3c275ec5ed5a6e7263c97da4c75236814b350ceb
Robert Rostohar <robert.rostohar@arm.com>
2022-06-06 08:44:52 +0200

    AWS MQTT/OTA Demo: add MDK-ARM project using GCC

---
+++ AWS_MQTT_OTA_TFM/Projects/app/sign_image.bat
@@ -0,0 +1,4 @@
+imgtool sign --version "1.0.0" --key ../bl2/root-RSA-3072_1.pem --public-key-format full --align 16 --pad --pad-header --boot-record NSPE --header-size 0x400 --slot-size 0xA0000 --security-counter 1 --confirm --dependencies "(0,"1.5.0"+0)" ./Objects/b_u585i_iot02a_tfm_ns.hex b_u585i_iot02a_tfm_ns_signed.bin
+imgtool sign --version "1.0.0" --key ../bl2/root-RSA-3072_1.pem --public-key-format full --align 16 --pad --pad-header --boot-record NSPE --header-size 0x400 --slot-size 0xA0000 --security-counter 1           --dependencies "(0,"1.5.0"+0)" ./Objects/b_u585i_iot02a_tfm_ns.hex b_u585i_iot02a_tfm_ns_ota.bin
+
+bin2hex.py --offset 0x0807A000 b_u585i_iot02a_tfm_ns_signed.bin b_u585i_iot02a_tfm_ns_signed.hex
